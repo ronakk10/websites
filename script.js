c65d51cdd7b47ad334f272fb8f23519fe5322b3a
ronakk10 <108456212+ronakk10@users.noreply.github.com>
2023-12-30 23:53:16 +0530

    proposal question

---
+++ script.js
@@ -0,0 +1,25 @@
+const wrapper = document.querySelector(".wraper");
+const question = document.querySelector(".question");
+const yesBtn = document.querySelector(".yes-btn");
+const noBtn = document.querySelector(".no-btn");
+const gif = document.querySelector(".gif-image");
+
+const wrapperReact = wrapper.getBoundingClientRect();
+const noBtnReact = noBtn.getBoundingClientRect();
+
+yesBtn.addEventListener("click", () => {
+  question.innerHTML = "I Love You Too :)";
+  noBtn.style.display = "none";
+  yesBtn.style.display = "none";
+  gif.innerHTML = `<img src="./cat_love.gif" alt="gif" />`;
+});
+
+noBtn.addEventListener("mouseover", () => {
+  const i =
+    Math.floor(Math.random() * (wrapperReact.width - noBtnReact.width)) + 1;
+  const j =
+    Math.floor(Math.random() * (wrapperReact.height - noBtnReact.height)) + 1;
+
+  noBtn.style.left = i + "px";
+  noBtn.style.top = j + "px";
+});
